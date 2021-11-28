92d7d0cfec3b73a110c15c218764659620c735bf
vijayg963 <vijayg963@gmail.com>
2021-11-28 12:44:11 +0530

    Manipulating-Elements

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,26 +6,45 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
+function createInputElm(labelText, type = "type") {
+  let input = document.createElement(`input`);
+  let label = document.createElement(`label`);
+  label.innerText = labelText;
+  label.append(input);
+  input.type = type;
+  return label;
+}
 
 // TEST
-createInputElm('Your name'); //<label>Your name: <input type="text"></label>
-createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
+createInputElm("Your name"); //<label>Your name: <input type="text"></label>
+createInputElm("Your age", "number"); //<label>Your age: <input type="number"></label>
 
 // 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
 
 // Your code goes here
+function heading(text) {
+  let heading1 = document.createElement(`h1`);
+  heading1.innerText = text;
+  return heading1;
+}
 
 // TEST
-createInputElm('Your name'); //<label>Your name: <input type="text"></label>
-createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
+createInputElm("Your name"); //<label>Your name: <input type="text"></label>
+createInputElm("Your age", "number"); //<label>Your age: <input type="number"></label>
 
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
 
+function createList(arr = []) {
+  let html = `<ul>
+  ${arr.map((elm) => `<li>${elm}</li>`).join("")} </ul>`;
+  return html;
+}
+
 // TEST
-createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
-createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
+createList(["ALABAMA", "ALASKA", "HAWAII", "KENTUCKY"]);
+createList(["Afghanistan", "Antarctica", "Congo", "Estonia"]);
 
 // 4. Create a function named `createTodoList` that accept and array of data like [{name: "Learn DOM", isDone: false}, {name: "Learn JS", isDone: true}] and returns
 // the html for single todo will look like given below
@@ -40,14 +59,26 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 */
 
 // Your code goes here
+function createTodoList(data = []) {
+  let html = `<ul>
+  ${data
+    .map(
+      (todo) => `<li>
+  <p>${todo.name}</p>
+  <input type="checkbox" ${todo.isDone ? "checked" : ""} name ="" id="">
+  <span></span></li>`
+    )
+    .join("")}</ul>`;
+  return html;
+}
 
 // TEST
 createTodoList([
-  { name: 'Learn DOM', isDone: false },
-  { name: 'Learn JS', isDone: true },
+  { name: "Learn DOM", isDone: false },
+  { name: "Learn JS", isDone: true },
 ]);
 createTodoList([
-  { name: 'Learn DOM', isDone: false },
-  { name: 'Learn React', isDone: true },
-  { name: 'Learn JS', isDone: true },
+  { name: "Learn DOM", isDone: false },
+  { name: "Learn React", isDone: true },
+  { name: "Learn JS", isDone: true },
 ]);
